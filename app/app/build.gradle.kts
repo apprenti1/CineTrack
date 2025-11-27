24ca41800844adb880e9406f03c2c70304536a1e
AITBOUKHA Elias <elias.aitboukha@gmail.com>
2025-11-27 09:19:32 +0100

    Ajouter permission internet et dépendance Coil

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")}
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+}
